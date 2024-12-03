0e875daa3f3deeecb0fdaf8ea56d6337d8d2857e
lariturini <larissahelena967@gmail.com>
2024-12-03 15:35:49 -0300

    Add files via upload

---
+++ Assets/perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Química',
+    'Qual elemento químico principal que forma todos os compostos organicos?',
+    'O Carbono',
+)
+
+criaCartao(
+    'Matematica',
+    'Qual a probabilidade de lançar um dado de 6 lados e obter um numero impar?',
+    'A probabilidade é de 50%' ,
+)
+
+criaCartao(
+    'Historia',
+    'Qual foi a guerra mais sangrenta que o Brasil já enfrentou?',
+    'A Guerra do Paraguai',
+)
+
+criaCartao(
+   'Portugues',
+   'Uma caracteristica do Barroco?',
+   'Uma das caracteristicas do Barroco é o exagero',
+)
+
+criaCartao(
+    'Sociologia',
+    ' O que é Racismo Ambiental?',
+    'O Racismo Ambiental é a forma como a degradação ambiental afeta de forma desproporcional grupos mais vulneraveis',
+ )
